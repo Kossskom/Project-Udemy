0e51a3eeded797256d4b1537b165596c85edcf43
Kossskom <ya.kom345@yandex.ru>
2022-09-06 16:53:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('No and yes');
+// console.log(result);
+
+// const answer = +prompt('How old are you', '');
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
